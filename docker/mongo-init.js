139f3c4778e6ece1cbd3b7aef082adf862ac0e94
Phil Palmieri <philpalmieri@me.com>
2025-09-27 15:12:30 -0400

    Test Setup

---
+++ docker/mongo-init.js
@@ -0,0 +1,25 @@
+// Initialize MongoDB with collections and indexes
+db = db.getSiblingDB('stellarburn');
+
+// Create collections
+db.createCollection('sectors');
+db.createCollection('players');
+db.createCollection('systems');
+
+// Create indexes for efficient 4D coordinate queries
+db.sectors.createIndex({ "coordinates": 1 });
+db.sectors.createIndex({ "dynamicObjects.ships": 1 });
+db.sectors.createIndex({ "lastActivity": 1 });
+
+// Create compound index for geospatial-style queries
+db.sectors.createIndex({ 
+  "coord.x": 1, 
+  "coord.y": 1, 
+  "coord.z": 1, 
+  "coord.w": 1 
+});
+
+db.players.createIndex({ "playerId": 1 });
+db.players.createIndex({ "currentCoordinates": 1 });
+
+print("StellarBurn database initialized!");
